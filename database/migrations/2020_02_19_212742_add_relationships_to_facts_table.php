69b4886d9f8cce7539bdb5c4096427a40ec2f8dd
Robert <rsztabralowski@gmail.com>
2020-02-20 02:16:18 +0000

    Added relayionship to facts table

---
+++ database/migrations/2020_02_19_212742_add_relationships_to_facts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRelationshipsToFactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('facts', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('facts', function (Blueprint $table) {
+            //
+        });
+    }
+}
